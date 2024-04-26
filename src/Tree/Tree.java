f050c29aa71d546e58c310bb3852e6e1df0fd177
Yves HAKIZIMANA <yvhakizimana123@gmail.com>
2024-04-26 10:18:08 +0200

    Adding Depth Traversal Algorithms

--- src/Tree/Tree.java
+++ src/Tree/Tree.java
@@ -71,4 +71,37 @@ public class Tree {
         }
         return false;
     }
+
+    public void traversePreOrder(){
+        traversePreOrder(root);
+    }
+
+    public void traverseInOrder(){
+        traverseInOrder(root);
+    }
+
+    public void traversePostOrder(){
+        traversePostOrder(root);
+    }
+    // Pre-order means root, left , child
+    private void traversePreOrder(Node root){
+        if(root == null) return;
+        System.out.println(root.data);
+        traversePreOrder(root.leftChild);
+        traversePreOrder(root.rightChild);
+    }
+
+    private void traverseInOrder(Node root){
+        if(root == null) return;
+        traverseInOrder(root.leftChild);
+        System.out.println(root.data);
+        traverseInOrder(root.rightChild);
+    }
+
+    private void traversePostOrder(Node root){
+        if(root == null) return;
+        traversePostOrder(root.leftChild);
+        traversePostOrder(root.rightChild);
+        System.out.println(root.data);
+    }
 }
